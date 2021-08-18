d871864db58fa9322887507b748424f4b7a53093
Leandro Bento <leandropro84@hotmail.com>
2021-08-18 10:11:50 -0300

    Commit Inicial da Aplicacao 18082021-10h09

---
+++ src/main/java/io/leandro/pontointeligenteapi/SistemaPontoInteligenteMacApplication.java
@@ -0,0 +1,13 @@
+package io.leandro.pontointeligenteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPontoInteligenteMacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPontoInteligenteMacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/leandro/pontointeligenteapi/SistemaPontoInteligenteMacApplicationTests.java
@@ -0,0 +1,13 @@
+package io.leandro.pontointeligenteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPontoInteligenteMacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
